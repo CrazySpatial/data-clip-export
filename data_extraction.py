29d9819b8bec76c5290bd4fc9827e7aa4d1931a3
CrazySpatial <johnmann111@gmail.com>
2016-08-17 15:16:52 -0400

    created a script to run inside the python window

--- data_extraction.py
+++ data_extraction.py
@@ -1,19 +1,15 @@
 #Name: data_extraction
 #Purpose: This tool is designed to run in the python module in arc. Add any and all features inside  the layer panel and then create a feature class 
 #                   named study_area. All features will export to a path designated by the user.
+import os, arcpy
+arcpy.env.overwriteOutput = True
 
-import arcpy
-from arcpy import env
-from arcpy import mapping
+in_clip_layer = r"L:/2016/test/study_area.shp" #Select  location of the study_area
+out_gdb = r"D:/output/" #Define a location to dump the files
 
-mxd = arcpy.mapping.MapDocument("CURRENT")  # Uses your currently open MXD
-df = arcpy.mapping.ListDataFrames(mxd, '')[0] # Chooses the first dataframe
+mxd_obj = arcpy.mapping.MapDocument("CURRENT") # This will create a object for the current map document in which y ou will use for you for loop
 
-destPath = r"D:/output" # Set your destination folder path
-
-for lyr in arcpy.mapping.ListLayers(mxd, '', df): # Loop through layers
-    # Output layer to shapefile
-        if lyr.name == 'study_area'
-            clip_area = lyr
-
-    arcpy.FeatureClassToFeatureClass_conversion(lyr, destPath, "{}.shp".format(lyr), "", "", "")
+for lyr in arcpy.mapping.ListLayers(mxd_obj):
+    arcpy.AddMessage(lyr)
+    out_layer = os.path.join(out_gdb,lyr.name)
+    arcpy.Clip_analysis(lyr,in_clip_layer,out_layer)
